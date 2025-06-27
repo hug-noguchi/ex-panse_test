90a54ca1e8831fba17a457ba41a9a155f70b424f
keito noguchi <noguchi@hug-inc.co.jp>
2025-06-27 10:34:09 +0900

    キャンペーンタイトル・日付修正

--- saka/functions.php
+++ saka/functions.php
@@ -201,17 +201,17 @@ add_action( 'pre_get_posts', 'change_posts_per_page' );
 function campaign_end_date() {
   date_default_timezone_set('Asia/Tokyo'); // タイムゾーンを設定
   $current_time = strtotime(date('Y-m-d H:i'));
-  $campaign_end_date = strtotime('2025-5-31 23:59');
+  $campaign_end_date = strtotime('2025-6-30 23:59');
 
   ob_start(); // 出力バッファリングを開始
 
   ?>
   <p class="campaign">
-      特別キャンペーン <br class="campaign_pc">期間限定<span>
+      夏の特別キャンペーン <br class="campaign_pc">期間限定<span>
       <?php if ($current_time < $campaign_end_date) { ?>
-          5/31
-      <?php } else { ?>
           6/30
+      <?php } else { ?>
+          7/31
       <?php } ?>
       </span>まで
   </p>
