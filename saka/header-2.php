0329e3095fcafe2814fa5559d35adee60a7cee93
keito noguchi <noguchi@hug-inc.co.jp>
2025-07-08 18:42:03 +0900

    タイトルとh1のテキスト取得修正

--- saka/header-2.php
+++ saka/header-2.php
@@ -18,10 +18,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?php wp_title('｜' , true , 'right'); ?></title>
+	<title><?php echo get_bloginfo('name'); ?>></title>
 <?php wp_head(); ?>
 	<meta name="keywords" content="アーユルヴェーダ,養成講座,セラピスト,スクール,インストラクター,マッサージ">
-	<meta name="description" content="<?php wp_title('｜' , true , 'right'); ?>アーユルヴェーダスクールです。東京・恵比寿駅徒歩１分。セラピストとして新たな技術や、これから独立をお考えの方への本場ケララのアーユルヴェーダスクール。実技・座学等、色々な養成講座コースがあります。独立支援コースやサロンの開業等サポートしております。東京・神奈川・千葉・茨城">
+	<meta name="description" content="<?php echo get_bloginfo('name'); ?>アーユルヴェーダスクールです。東京・恵比寿駅徒歩１分。セラピストとして新たな技術や、これから独立をお考えの方への本場ケララのアーユルヴェーダスクール。実技・座学等、色々な養成講座コースがあります。独立支援コースやサロンの開業等サポートしております。東京・神奈川・千葉・茨城">
 	<link rel="icon" href="images/icon.gif" type="image/jpeg" sizes="16x16">
 	<link rel="apple-touch-icon-precomposed" href="<?php bloginfo('url'); ?>/images/webclip.png" />
 	<link href="<?php bloginfo('url'); ?>/include/css/bootstrap.min.css" rel="stylesheet">
@@ -80,7 +80,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 		<header>
 			<div class="header_pc pc" style="display: none;">
 				<nav class="hd_nav">
-					<h1><?php wp_title("｜" , true , "right"); ?></h1>
+					<h1><?php echo get_bloginfo('name'); ?></h1>
 					<p class="logo"><a href="<?php bloginfo('url'); ?>"><picture><source type="image/webp" srcset="<?php bloginfo('url'); ?>/images/logo.webp"><img src="<?php bloginfo('url'); ?>/images/logo.png"></picture></a></p>
 					<ul class="hd_nav__list main">
 						<li class="hd_nav__item"><a href="<?php bloginfo('url'); ?>/concept/" class="btn_fade"><span>CONCEPT</span>スクールの特長</a></li>
@@ -97,7 +97,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 			</div>
 
 			<div class="header sp">
-				<h1><?php wp_title("｜" , true , "right"); ?></h1>
+				<h1><?php echo get_bloginfo('name'); ?></h1>
 				<nav class="global-nav">
 					<p class="logo"><a href="<?php bloginfo('url'); ?>"><picture><source type="image/webp" srcset="<?php bloginfo('url'); ?>/images/logo.webp"><img src="<?php bloginfo('url'); ?>/images/logo.png"></picture></a></p>
 					<ul class="global-nav__list">
